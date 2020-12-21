ad5550450f26fe9b6e08ea1437a766665d2e1862
Robert Alberto <arobertadrian@gmail.com>
2020-12-21 12:38:24 -0500

    Task: JSON code done

--- pythonjsontest.py
+++ pythonjsontest.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 import requests
-import json
-url = "http://openlibrary.org/search.json?q=9780684867625"
+url = "https://robertalberto.com/123.json"
 res = requests.get(url)
 scone = res.json()
-for key, value in scone.items():
-    print("---------------->")
-    print(value, key)
+print(type(scone['docs']))
+lst = scone['docs']
+for item in lst:
+    isbn = item['isbn']
+    title = item['title']
+    author = item['author_name']
+    lang = item['language']
+    genre = item['subject']
+    publisher = item['publisher']
+    #test onlu
+    print('\n',isbn[0],"\n",title,"\n",author[0],"\n",lang[0],"\n",genre[0],"\n",publisher[0],"\n")
